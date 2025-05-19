0339393c8474693d83b61b5cfc850ddb1a79809f
josep42ny <82282886+josep42ny@users.noreply.github.com>
2025-05-19 23:09:48 +0200

    refactor: stick to SRP and make GameLoader more readable

--- src/josep42ny/terminoes/GameLoader.java
+++ src/josep42ny/terminoes/GameLoader.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 public class GameLoader {
-
+    private static final int MAX_GAME_MODES = 7;
     private final GameDAO gameDAO;
     private final int[] GAME_INDEXES = new int[]{0, 1, 2, 3, 4, 5, 6};
 
@@ -20,46 +20,19 @@ public class GameLoader {
     }
 
     public void awake() {
-        Map<String, Game> saves = gameDAO.loadAll();
-        boolean wantsToLoad = false;
-
+        Map<String, Game> savedGames = gameDAO.loadAll();
+        boolean shouldLoadSavedGame = hasSavedGames(savedGames) && InputHandler.askLoad();
         View.homeScreen();
-        if (foundSaveIn(saves)) {
-            wantsToLoad = InputHandler.askLoad();
-        }
 
-        View.homeScreen();
-        if (wantsToLoad) {
-            View.drawSavedGames(saves);
-            int selection = InputHandler.askConstrainedInt(getSaveIndexes(saves), "Selecciona un mode [0-6]: ");
-            String saveName = saves.keySet().toArray()[selection].toString();
-            Game game = saves.get(saveName);
-            Ansi.clearScreen();
-            game.resumeGame();
-            gameDAO.delete(saveName);
+        if (shouldLoadSavedGame) {
+            loadSavedGame(savedGames);
         } else {
-            View.drawNewGames();
-            int selection = InputHandler.askConstrainedInt(GAME_INDEXES, "Selecciona un mode [0-6]: ");
-
-            Game game = switch (selection) {
-                case 0 -> new GameSpanish(0);
-                case 1 -> new GameMexican(1);
-                case 2 -> new GameLatino(2);
-                case 3 -> new GameColombian(3);
-                case 4 -> new GameChilean(4);
-                case 5 -> new GameVenezuelan(5);
-                case 6 -> new GamePonce(6);
-                default -> throw new IllegalStateException("Unexpected value: " + selection);
-            };
-
-            Ansi.clearScreen();
-            game.startGame();
+            startNewGame();
         }
     }
 
-
-    private boolean foundSaveIn(Map<String, Game> saves) {
-        for (Map.Entry<String, Game> entry : saves.entrySet()) {
+    private boolean hasSavedGames(Map<String, Game> savedGames) {
+        for (Map.Entry<String, Game> entry : savedGames.entrySet()) {
             if (entry.getValue() != null) {
                 return true;
             }
@@ -67,10 +40,10 @@ public class GameLoader {
         return false;
     }
 
-    private int[] getSaveIndexes(Map<String, Game> saves) {
+    private int[] extractSavedGameIndexes(Map<String, Game> savedGames) {
         List<Integer> indexes = new ArrayList<>();
         int index = 0;
-        for (Map.Entry<String, Game> entry : saves.entrySet()) {
+        for (Map.Entry<String, Game> entry : savedGames.entrySet()) {
             if (entry.getValue() != null) {
                 indexes.add(index);
             }
@@ -79,4 +52,32 @@ public class GameLoader {
         return indexes.stream().mapToInt(i -> i).toArray();
     }
 
+    private void loadSavedGame(Map<String, Game> savedGames) {
+        View.drawSavedGames(savedGames);
+        int selection = InputHandler.askConstrainedInt(extractSavedGameIndexes(savedGames), "Selecciona un mode [0-" + (MAX_GAME_MODES - 1) + "]: ");
+        String saveName = (String) savedGames.keySet().toArray()[selection];
+        Game game = savedGames.get(saveName);
+
+        Ansi.clearScreen();
+        game.resumeGame();
+        gameDAO.delete(saveName);
+    }
+
+    private void startNewGame() {
+        View.drawNewGames();
+        int selection = InputHandler.askConstrainedInt(GAME_INDEXES, "Selecciona un mode [0-" + (MAX_GAME_MODES - 1) + "]: ");
+        Game game = switch (selection) {
+            case 0 -> new GameSpanish(0);
+            case 1 -> new GameMexican(1);
+            case 2 -> new GameLatino(2);
+            case 3 -> new GameColombian(3);
+            case 4 -> new GameChilean(4);
+            case 5 -> new GameVenezuelan(5);
+            case 6 -> new GamePonce(6);
+            default -> throw new IllegalStateException("Unexpected value: " + selection);
+        };
+
+        Ansi.clearScreen();
+        game.startGame();
+    }
 }
